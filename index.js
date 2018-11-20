dc3bfdf953fcbe367ec7371e8c3d7d1be7657b8e
Ally Netzel <netzelally@gmail.com>
2018-11-20 20:44:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ randomNumber = Math.floor(Math.random() * 20) + 0;
 randomInteger = Math.round(randomNumber)
 const newID = oldID + 1000000000;
 const ageIsValid = Number.isInteger(currentAge);
-const randomUserID = 
+const randomUserID = Math.floor(Math.random()  * 1000000020) + 1000000001;
